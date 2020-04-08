c2a31f8aad5b155eb5a208a3ff1de631639072e7
a1773620 <a1773620@29f0dbc1-fc81-47ad-95d7-fb0db5eeff69>
2020-04-08 16:08:38 +0000

    Fixing 1-1

--- 2020/s1/oop/practical-04/function-1-1.cpp
+++ 2020/s1/oop/practical-04/function-1-1.cpp
@@ -6,9 +6,17 @@ void copy_2d_strings(string first[][2], string second[][2], int n){
 	int i=0;
 	int j=0;
 
-	for(i=0;i<3;i++){
-		for(j=0;j<n;j++){
+	for(i=0;i<n;i++){
+		for(j=0;j<3;j++){
 			second[i][j]=first[i][j];
 		}
 	}
+
+	// for(i=0;i<n;i++){
+	// 	for(j=0;j<3;j++){
+	// 		std::cout << second[i][j] << " ";
+	// 	}
+	// 	std::cout << std::endl;
+	// }
+
 }
