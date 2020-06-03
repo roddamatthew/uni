53e93b56148e918b62e838af3ca179709b4d948a
a1773620 <a1773620@29f0dbc1-fc81-47ad-95d7-fb0db5eeff69>
2020-06-03 02:33:32 +0000

    renaming animal and hunter files

--- 2020/s1/oop/workshop-11/main.cpp
+++ 2020/s1/oop/workshop-11/main.cpp
@@ -1,19 +1,19 @@
-#include "Animal.h"
+#include "animal.h"
 #include <iostream>
 #include <string>
 
 using namespace std;
 
 int main(){
-	Animal* panda;
-	Animal* fox;
+	animal* panda;
+	animal* fox;
 
 
-	panda = new Animal("Panda", 100);
-	fox = new Animal("Fox", 45);
+	panda = new animal("Panda", 100);
+	fox = new animal("Fox", 45);
 
-	std::cout << "Animal with ID: " << panda -> get_animalID() << " has the name: " << panda -> get_name() << " and a volume of: " << panda -> get_volume() << std::endl;
-	std::cout << "Animal with ID: " << fox -> get_animalID() << " has the name: " << fox -> get_name() << " and a volume of: " << fox -> get_volume() << std::endl;
+	std::cout << "animal with ID: " << panda -> get_animalID() << " has the name: " << panda -> get_name() << " and a volume of: " << panda -> get_volume() << std::endl;
+	std::cout << "animal with ID: " << fox -> get_animalID() << " has the name: " << fox -> get_name() << " and a volume of: " << fox -> get_volume() << std::endl;
 
 
 	return 0;
