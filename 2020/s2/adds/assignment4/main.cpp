dbb3f52596b310597ac698080f948bba5bca6b9d
a1773620 <a1773620@29f0dbc1-fc81-47ad-95d7-fb0db5eeff69>
2020-08-31 07:30:00 +0000

    Recursion prac checking input in main version

--- 2020/s2/adds/assignment4/main.cpp
+++ 2020/s2/adds/assignment4/main.cpp
@@ -19,7 +19,21 @@ int main(){
 	std::cin >> numCrates;
 	std::cin >> loadSize;
 
-	std::cout << R.reverseDigit(i) << " " << R.reverseString(s) << " " << T.numTrucks(numCrates,loadSize) << " " << E.numTrucks(numCrates,loadSize) << " " << std::endl;
+	if(i < 0){
+		std::cout << "ERROR ";
+	}else{
+		std::cout << R.reverseDigit(i) << " ";
+	}
+
+	std::cout << R.reverseString(s) << " ";
+
+	if(numCrates < 2 || numCrates > 10000){
+		std::cout << "ERROR ERROR";
+	}else if(loadSize < 1 || loadSize >= numCrates){
+		std::cout << "ERROR ERROR";
+	}else{
+		std::cout << T.numTrucks(numCrates,loadSize) << " " << E.numTrucks(numCrates,loadSize) << " " << std::endl;
+	}
 
 	return 0;
 }
