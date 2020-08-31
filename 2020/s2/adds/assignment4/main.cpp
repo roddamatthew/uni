9376e85c92388ba29a578d44ceb6e0c2a4b0efd0
a1773620 <a1773620@29f0dbc1-fc81-47ad-95d7-fb0db5eeff69>
2020-08-31 08:17:50 +0000

    checking first input properly

--- 2020/s2/adds/assignment4/main.cpp
+++ 2020/s2/adds/assignment4/main.cpp
@@ -15,6 +15,13 @@ int main(){
 	EfficientTruckloads E;
 
 	std::cin >> i;
+
+	if (std::cin.fail()) {
+		i = 0;
+		std::cin.clear(); //clear bad input flag
+        std::cin.ignore(); //discard input
+	}
+
 	std::cin >> s;
 	std::cin >> numCrates;
 	std::cin >> loadSize;
@@ -24,13 +31,13 @@ int main(){
 	}else{
 		std::cout << "ERROR ";
 	}
-
+		 
 	std::cout << R.reverseString(s) << " ";
 
 	if(numCrates > 2 && loadSize >= 1){
 		std::cout << T.numTrucks(numCrates,loadSize) << " " << E.numTrucks(numCrates,loadSize) << " " << std::endl;
 	}else{
-		std::cout << "ERROR ERROR";
+		std::cout << "ERROR ERROR" << std::endl;
 	}
 
 	return 0;
