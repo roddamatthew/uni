ff927e03a5dfcef1c2047228c047018c2bd34fc7
a1773620 <a1773620@29f0dbc1-fc81-47ad-95d7-fb0db5eeff69>
2020-09-03 02:18:48 +0000

    All but question 11 complete

--- 2020/s2/cs/workshop06/question08.asm
+++ 2020/s2/cs/workshop06/question08.asm
@@ -7,4 +7,13 @@
 	@numbers
 	@arr
 // add your instructions after this line
-
+	// x = arr[y]
+	// First get arr[y]
+	@arr
+	D=M	// Storing arr in the D register
+	@y
+	A=D+M
+	D=M
+	// Now have arr[y] stored in the D register
+	@x
+	M=D
