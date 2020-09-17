38939bfc60b867b21265b1a54d1582dd1f2afeed
a1773620 <a1773620@29f0dbc1-fc81-47ad-95d7-fb0db5eeff69>
2020-09-17 10:26:54 +0000

    Prac exam 2 practice question

---
+++ 2020/s2/adds/pracexam2practice/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+extern int sumDigits(long n);
+
+int main()
+{
+	std::cout << sumDigits( 123 ) << std::endl ;
+	
+	return 0 ;
+}
---
+++ 2020/s2/adds/pracexam2practice/sumDigits.cpp
@@ -0,0 +1,6 @@
+int sumDigits(long n)
+{
+	if( n == 0 ) return 0 ;
+
+	return ( n % 10 + sumDigits( n / 10 ) ) ;
+}
