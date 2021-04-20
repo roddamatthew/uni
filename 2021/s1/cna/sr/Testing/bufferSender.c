67effcae06e7d90430270d178271c48771c0376f
a1773620 <a1773620@29f0dbc1-fc81-47ad-95d7-fb0db5eeff69>
2021-04-20 07:56:29 +0000

    Sender first implementation complete. Filled in skeleton functions and added some handling for corrupted packets. Need to add handling for corrupted ACKs receieved at the sender (in A_input). A_timerinterrupt also needs to be tested fully before first submission.

--- 2021/s1/cna/sr/Testing/bufferSender.c
+++ 2021/s1/cna/sr/Testing/bufferSender.c
@@ -1,8 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include "emulator.h"
-#include "sr.h"
 
 /* ******************************************************************
 	Selective Repeat Protocol:...
@@ -13,6 +11,10 @@
 #define SEQSPACE 12             /* min sequence space for SR is 2 * WINDOWSIZE */
 #define NOTINUSE (-1)           /* used to fill header fields that are not being used */
 #define A 0
+
+static int timercount = 0 ;
+static int window_full = 0 ;
+static int TRACE = 0 ;
 
 /* generic procedure to compute the checksum of a packet.  Used by both sender and receiver  
    the simulator will overwrite part of your packet with 'z's.  It will not overwrite your 
@@ -68,6 +70,22 @@ bool withinRange( int lower, int upper, int val ) {
   }
 }
 
+/* Fake starttimer function so code compiles */
+void starttimer( int i, int j ) {
+  printf( "Started a timer number: %d\n", timercount ) ;
+  timercount++ ;
+}
+
+/* Fake stoptimer function so code compiles */
+void stoptimer( int i ){
+  printf( "Stoped a timer number: %d\n", timercount ) ;
+}
+
+/* Fake tolayer3 function so code compiles */
+void tolayer3( int i, struct pkt packet ) {
+  printf( "Sent a packet to layer 3 with seqnum: %d\n", packet.seqnum ) ;
+}
+
 /********* Sender (A) variables and functions ************/
 
 /* Create a struct for the sender buffer */
@@ -93,45 +111,115 @@ struct senderBufferUnit {
 static struct senderBufferUnit senderBuffer[ WINDOWSIZE ] ;
 static int timerAssociatedSeqNum ;
 static bool timerStarted ;
+static int sendBase ;
+static int nextSeqNum ;
 
 /* Create a packet, filling in the correct seqnum, and return it */
 struct pkt createPacket( struct msg message ) {
   struct pkt packet ;
+  int i ;
+
+  packet.seqnum = nextSeqNum ;
+  packet.acknum = NOTINUSE ;
+
+  nextSeqNum = ( nextSeqNum + 1 ) % SEQSPACE ;
+
+  for ( i = 0; i < 20 ; i++ )
+    packet.payload[ i ] = message.data[ i ] ;
+  packet.checksum = ComputeChecksum( packet ) ;
+
   return packet ;
 }
 
+/* return true if the sender window is full */
+bool windowFull() {
+  int i ;
+  int windowCount = 0 ;
+
+  for( i = 0 ; i < WINDOWSIZE ; i++ ) {
+    if( senderBuffer[i].sent == true ) windowCount++ ;
+  }
+
+  if( windowCount == WINDOWSIZE ) return true ;
+  else return false ;
+}
+
 /* add a packet to the end of the buffer */
-void addPacketToBuffer( struct pkt packet ) {
-  /* if sender window is full, return an error */
-}
-
-/* Fake starttimer function so code compiles */
-void starttimer( int A, int RTT ) {
-
-}
-
-/* Fake stoptimer function so code compiles */
-void stoptimer( int A ){
-
+void addPacketToBuffer( struct pkt newPacket ) {
+  int index = newPacket.seqnum - sendBase ;
+  senderBuffer[ index ].sent = true ;
+  senderBuffer[ index ].packet = newPacket ;
 }
 
 /* Update buffer to have received ack for given acknum */
 void bufferReceiveACK( int acknum ) {
-
+  int index = acknum - sendBase ;
+  senderBuffer[ index ].acked = true ;
 }
 
 /* move senderBuffer window across as long as the zeroth packet is acked */
 void moveSenderWindow() {
+  while( senderBuffer[0].acked == true ) {
+    int i ;
+    /* Cycle the elements of the buffer down one position */
+    for( i = 0 ; i < WINDOWSIZE - 1 ; i++ ) {
+      senderBuffer[ i ].packet = senderBuffer[ i + 1 ].packet ;
+      senderBuffer[ i ].acked = senderBuffer[ i + 1 ].acked ;
+      senderBuffer[ i ].sent = senderBuffer[ i + 1 ].sent ;
+    }
 
+    /* Increase the base of the sender */
+    sendBase = ( sendBase + 1 ) % SEQSPACE ;
+
+    /* empty the last element in the buffer */
+    senderBuffer[ WINDOWSIZE - 1 ].acked = false ;
+    senderBuffer[ WINDOWSIZE - 1 ].sent = false ;
+  }
+}
+
+/* Start the sender timer and update the associated state variables */
+/* Input parameter is the seqeunce number of the packet starting the timer */
+void startTimer( int seqnum ) {
+  /* If called with an invalid sequence number spit an error */
+  if( seqnum >= 0 ) {
+    starttimer( A, RTT ) ;
+    timerAssociatedSeqNum = seqnum ;
+    timerStarted = true ;
+  } else {
+    printf( "startTimer called with an invalid seqnuence number: %d (note: -1 denotes a seqnum not in use)\n", seqnum ) ;
+  }
+}
+
+/* Stop the timer and update the associated state variables */
+void stopTimer() {
+  stoptimer( A ) ;
+  timerAssociatedSeqNum = NOTINUSE ;
+  timerStarted = false ;
 }
 
 /* called from layer 5 (application layer), passed the message to be sent to other side */
 void A_output(struct msg message)
 {
-  struct pkt packet ;
-  packet = createPacket( message ) ;
-  addPacketToBuffer( packet ) ;
-  if( !timerStarted() ) starttimer( A, RTT ) ;
+  if( !windowFull() ) {
+    /* Create a packet and sent it to the network layer */
+    struct pkt packet ;
+    packet = createPacket( message ) ;
+    tolayer3( A, packet ) ;
+
+    /* Add the packet to our senderBuffer */
+    addPacketToBuffer( packet ) ;
+
+    /* If a timer is not started, start one */
+    if( !timerStarted ) {
+      startTimer( packet.seqnum ) ;
+    }
+  } else {
+    /* New message arrived at transport layer, but our send window was already full */
+    /* Give a useful error message */
+    if (TRACE > 0)
+      printf("----A: New message arrives, send window is full\n");
+    window_full++;
+  }
 }
 
 
@@ -140,21 +228,35 @@ void A_output(struct msg message)
 */
 void A_input( struct pkt packet )
 {
-  /* Update buffer to have received ACK */
-  bufferReceiveACK( packet.acknum ) ;
-  /* Stop timer if ack was associated with timer */
-  if( timerAssociatedSeqNum == packet.acknum ) stoptimer( A ) ;
-  /* Move window if necessary */
-  moveSenderWindow() ;
+  if( !IsCorrupted( packet ) ){
+    /* Update buffer to have received ACK */
+    bufferReceiveACK( packet.acknum ) ;
+    /* Stop timer if ack was associated with timer */
+    if( timerAssociatedSeqNum == packet.acknum ) stopTimer() ;
+    /* Move window if necessary */
+    moveSenderWindow() ;
+  }
 }
 
 /* called when A's timer goes off */
 void A_timerinterrupt(void)
 {
+  int i ;
+  int firstResentSeqNum = -1 ;
   /* Resend all packets not yet acked */
-  for()... {}
+  for( i = 0 ; i < WINDOWSIZE ; i++ ) {
+    /* Check that the packet is also initialized */
+    if( senderBuffer[i].acked == false && senderBuffer[i].packet.seqnum != NOTINUSE ) {
+      if( firstResentSeqNum == -1 ) firstResentSeqNum = senderBuffer[i].packet.seqnum ;
+      /* Resend the packet not acked */
+      tolayer3( A, senderBuffer[i].packet ) ;
+      /* Make sure the buffer knows this packet has been sent */
+      senderBuffer[i].sent = true ;
+    }
+  }
+
   /* Restart timer */
-  starttimer( A, RTT ) ;
+  startTimer( firstResentSeqNum ) ;
 }       
 
 
@@ -163,12 +265,60 @@ void A_timerinterrupt(void)
 /* entity A routines are called. You can use it to do any initialization */
 void A_init(void)
 {
+  int i ;
   /* Initialize senderBuffer */
-  for() ... ;
+  for( i = 0 ; i < WINDOWSIZE ; i++ ) {
+    senderBuffer[i].packet.seqnum = NOTINUSE ;
+    senderBuffer[i].packet.acknum = NOTINUSE ;
+    senderBuffer[i].acked = false ;
+    senderBuffer[i].sent = false ;
+  }
+
   timerAssociatedSeqNum = NOTINUSE ;
   timerStarted = false ;
+  nextSeqNum = 0 ;
+  sendBase = 0 ;
+}
+
+void printSenderBuffer() {
+  int i, j ;
+  printf( "senderBuffer: \n" ) ;
+  for( i = 0 ; i < WINDOWSIZE ; i++ ) {
+    printf( "sent: %s\n", senderBuffer[i].acked ? "true" : "false" ) ;
+    printf( "acked: %s\n", senderBuffer[i].acked ? "true" : "false" ) ;
+    printf( "packet: seqnum: %d, acknum: %d\n", senderBuffer[i].packet.seqnum, senderBuffer[i].packet.acknum ) ;
+    printf( "payload: " ) ;
+    for( j = 0 ; j < 20 ; j++ ) {
+      printf( "%c", senderBuffer[i].packet.payload[j] ) ;
+    }
+    printf( "\n" ) ;
+  }
+  printf( "\n" ) ;
 }
 
 int main() {
+  struct msg message ;
+  int i ;
+
+  for( i = 0 ; i < 20 ; i++ ) {
+    message.data[i] = 'a' ;
+  }
+
+  struct pkt packet ;
+  packet.acknum = 0 ;
+  packet.seqnum = NOTINUSE ;
+  for( i = 0 ; i < 20 ; i++ ) {
+    packet.payload[i] = '0' ;
+  }
+  packet.checksum = ComputeChecksum( packet ) ;
+
+  A_init() ;
+  A_output( message ) ;
+  A_output( message ) ;
+  A_output( message ) ;
+  A_output( message ) ;
+  A_input( packet ) ;
+  printSenderBuffer() ;
+
   return 0 ;
 }
