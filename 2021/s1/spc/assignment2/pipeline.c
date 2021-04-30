75d9e5ca385576ae79f6a60f767806f555d3f7c6
a1773620 <a1773620@29f0dbc1-fc81-47ad-95d7-fb0db5eeff69>
2021-04-30 07:12:30 +0000

    Changed readCommands() function in pipeline.c to use strok function after receiving some advice from Bernard. This may fix the issue I was having with one of the test cases.

--- 2021/s1/spc/assignment2/pipeline.c
+++ 2021/s1/spc/assignment2/pipeline.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -13,123 +12,28 @@
 #define MAXCMDS 100 /* maximum number of commands from a file */
 #define MAXCHARS 256 /* maximum number of characters per line */
 
-/* global variable for debugging */
-/* set greater than 1 to check the behaviour at each step */
-static int TRACE = 0 ;
-
 /* global variables for storing our commands */
 /* each element is a string */
 /* each row is a command */
 /* each column is an argument */
 static char* arguments[ MAXCMDS ][ MAXARGS ] ;
 
-/* return a copy of the input string */
-/* end the input string when the first space is read, or we reach the end of the string */
-char* copyStringUntilSpace( char* string ) {
-	int i ;
-	char* copy = malloc( strlen( string ) + 1 ) ;
-	strcpy( copy, string ) ;
-
-	for( i = 0 ; i < strlen( copy ) ; i++ ) {
-		if( copy[i] == ' ' ) copy[i] = '\0' ; else
-		if( copy[i] == 10  ) copy[i] = '\0' ; else
-		if( copy[i] == 0   ) copy[i] = '\0' ;
-	}
-
-	return copy ;
-}
-
-/* Copy all characters in a string after the first space is read */
-/* This is used to remove arguments from a line one at a time */
-char* copyStringAfterSpace( char* string ) {
-	int i ;
-	int charsBeforeSpace = 0 ;
-	int charsAfterSpace = 0 ;
-	bool readSpace = false ;
-	char* copy ;
-	
-	/* loop over the string */
-	for( i = 0 ; i < strlen( string ) ; i++ ) {
-		/* find how many characters are before and after the first space */
-		if( string[i] == ' ' && readSpace == false ) {
-			readSpace = true ;
-			charsBeforeSpace++ ;
-		}
-		else if( readSpace == true ) charsAfterSpace++ ; else
-		charsBeforeSpace++ ;
-	}
-
-	/* Allocate memory for a new string */
-	copy = malloc( charsAfterSpace + 1 ) ;
-	memcpy( copy, &string[ charsBeforeSpace ], charsAfterSpace ) ;
-	copy[ charsAfterSpace ] = '\0' ;
-
-	return copy ;
-}
-
-/* returns the number of 'words' in the string */
-/* read through the whole string and return the number of spaces + 1 */
-int nWords( char* string ) {
-	int i = 0 ;
-	int n = 0 ;
-
-	while( string[i] != 10 && string[i] != 0 && i <=strlen( string ) ) {
-		if( string[i] == ' ' ) n++ ;
-		i++ ;
-	}
-	/* final word will not have a space after it */
-	n++ ;
-
-	return n ;
-}
-
 /* Read input into our arguments array */
 void readCommands() {
-	int i, j ;
-	char input[ MAXCHARS ] ;
-	char* inputPtr ;
+	char input[80] ;
+	const char* space = " " ;
+	int i = 0, j = 0 ;
+	char* token ;
 
-	/* initialize both arrays to be filled with NULL */
-	for( i = 0 ; i < MAXCMDS ; i++ ) {
-		for( j = 0 ; j < MAXARGS ; j++ ) {
-			arguments[i][j] = NULL ;
-		}
-	}
-
-	/* Reset j = 0 */
-	j = 0 ;
-
-	/* Loop over the whole input or until we've read our max number of commands */
 	while( fgets( input, MAXCHARS, stdin ) != NULL && j < MAXCMDS ) {
+		token = strtok( input, space ) ;
 
-		/* Display the input for debugging */
-		if( TRACE > 0 ) {
-			printf( "input: %s\n", input ) ;
-			printf( "input as decimal: \n" ) ;
-			
-			/* Display characters as ASCII decimals */
-			for( i = 0 ; i < MAXCHARS ; i++ ) {
-				printf( "%d ", input[i] ) ;
-			}
-			printf( "\n" ) ;
+		while( token != NULL && i < MAXARGS ) {
+			printf( "%s\n", token ) ;
+			arguments[j][i] = token ;
+			token = strtok( NULL, space ) ;
+			i++ ;
 		}
-
-		/* Set our temporary pointer equal to our input's address*/
-		inputPtr = input ;
-		/* loop over each word in the input line */
-		for( i = 0 ; i < nWords( input ) ; i++ ) {
-			/* add the first word of the input to args array */
-			arguments[j][i] = copyStringUntilSpace( inputPtr ) ;
-
-			/* move the input string across to the next word */
-			inputPtr = copyStringAfterSpace( inputPtr ) ;
-			if( TRACE > 0 ) 
-				printf( "argument %d: %s\n", i, arguments[j][i] ) ;
-		}
-
-		if( TRACE > 0 ) 
-			printf( "\n" ) ;
-
 		/* last element of arguments string must be NULL for execvp */
 		arguments[ j ][ i ] = NULL ;
 		/* increase our commands counter */
