625901d91978c4ba478698fff0c2184faa36c3b7
a1773620 <a1773620@29f0dbc1-fc81-47ad-95d7-fb0db5eeff69>
2021-08-14 12:34:47 +0000

    Commenting on rsa and rotate

---
+++ 2021/s2/sp/assignment1/a1773620/rotate.c
@@ -0,0 +1,28 @@
+#include <stdint.h>
+#include <stdio.h>
+
+/* Rotate a 32 bit value count times */
+uint32_t rotate( uint32_t number, uint32_t count )
+{
+    for( int i = 0 ; i < count ; i++ )
+    {
+        /* If number > 2^31, then the 32nd bit must be 1, so shift in a 1 */
+        if( number > 2147483648 )
+        {
+            /* Bitwise shift the number and add 1 */
+            number = number << 1 ;
+            number++ ;
+        }
+        /* Otherwise, shift in a zero */
+        else number = number << 1 ;
+    }
+
+    return number ;
+}
+
+int main() {
+    uint32_t n = 10 ;
+    printf( "%d\n", rotate( n, 1 ) ) ;
+
+    return 0 ;
+}
