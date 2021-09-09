edbed8b87bc96aef956f6e3ff768faaef256da03
a1773620 <a1773620@29f0dbc1-fc81-47ad-95d7-fb0db5eeff69>
2021-09-09 06:42:14 +0000

    Check if there is error in LL case

--- 2021/s2/adsa/assignment2/main.cpp
+++ 2021/s2/adsa/assignment2/main.cpp
@@ -143,13 +143,13 @@ Node *balance_remove( Node *start )
     int balance = getBalance( start ) ;
 
     // Left left case:
-    if( balance > 1 && getBalance( start -> lower ) >= 0 && start -> lower != NULL )
-        return rightRotation( start ) ;
-    // // Left right case:
-    // else if( balance > 1 && getBalance( start -> lower ) < 0 ) {
-    //     start -> lower = leftRotation( start -> lower ) ;
+    // if( balance > 1 && getBalance( start -> lower ) >= 0 && start -> lower != NULL )
     //     return rightRotation( start ) ;
-    // }
+    // // Left right case:
+    if( balance > 1 && getBalance( start -> lower ) < 0 ) {
+        start -> lower = leftRotation( start -> lower ) ;
+        return rightRotation( start ) ;
+    }
     // Right right case:
     else if( balance < -1 && getBalance( start -> upper ) <= 0 && start -> upper != NULL ) {
         return leftRotation( start ) ;
